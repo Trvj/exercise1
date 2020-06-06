48017ac033e3a8bfb6d51a34be2d076adefa6cfa
traj112@hotmail.com <traj112@hotmail.com>
2020-06-06 11:53:14 -0500

    pv update

--- manifests/disk_management/pv.pp
+++ manifests/disk_management/pv.pp
@@ -4,7 +4,7 @@
 class exercise1::disk_management::pv {
 	require exercise1::disk_management::disk
 	# Create a hash from Hiera Data with the Users
-	$myUsers = hiera('exercise1::pv', {})
+	$pv = hiera('exercise1::pv', {})
 	# With Create Resource Converts a hash into a set of resources
 	create_resources(pv, $pv)
 }
