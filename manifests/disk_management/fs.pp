50b24f8739170c803b3472050008e1e0f446efc5
traj112@hotmail.com <traj112@hotmail.com>
2020-06-06 11:51:36 -0500

    pv update

--- manifests/disk_management/fs.pp
+++ manifests/disk_management/fs.pp
@@ -4,7 +4,20 @@
 class exercise1::disk_management::fs {
 	require exercise1::disk_management::lvm
 	# Create a hash from Hiera Data with the Users
-	$myUsers = hiera('exercise1::fs', {})
+	$fs = hiera('exercise1::fs', {})
 	# With Create Resource Converts a hash into a set of resources
 	create_resources(fs, $fs)
-}
+ 	
+ 	$ = hiera('exercise1::directory', {})
+	create_resources(file, $mydir)
+
+	mount { '/exercise':
+		ensure => mounted,
+		pass   => 0,
+		dump   => 0,
+		fstype => 'xfs',
+		options => 'defaults',
+		device => '/dev/exercisevg/exercise.fs',
+		require => Filesystem['/dev/exercisevg/exercise.fs']
+	}
+}		
