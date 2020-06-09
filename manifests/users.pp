46a8633be15b77d630dd83ffea281f642a5038bb
Tracy Johnson <traj112@hotmail.com>
2020-06-08 19:27:22 -0500

    update

--- manifests/users.pp
+++ manifests/users.pp
@@ -2,21 +2,12 @@
 #
 #
 class exercise1::users {
-	group { 'Andrews_PGroup':
-		gid => '2021',
-	}
+	$groups = hiera('exercise1::groups', {})
+	create_resources(group, $groups)
+ 
+	->
 	# Create a hash from Hiera Data with the Users
 	$users = hiera('exercise1::users', {})
 	# With Create Resource Converts a hash into a set of resources
 	create_resources(user, $users)
-}
-
-class exercise1::users {
-	group { 'Andrews_NPGroup':
-		gid => '2020',
-	}
-	# Create a hash from Hiera Data with the Users
-	$users = hiera('exercise1::users', {})
-	# With Create Resource Converts a hash into a set of resources
-	create_resources(user, $users)
-}
+ }
