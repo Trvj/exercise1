191f98b993200729bbe27b67d8b3cc23c921a061
Tracy Johnson <traj112@hotmail.com>
2020-06-08 19:28:37 -0500

    update

--- manifests/users.pp
+++ manifests/users.pp
@@ -5,7 +5,7 @@ class exercise1::users {
 	$groups = hiera('exercise1::groups', {})
 	create_resources(group, $groups)
  
-	->
+	
 	# Create a hash from Hiera Data with the Users
 	$users = hiera('exercise1::users', {})
 	# With Create Resource Converts a hash into a set of resources
