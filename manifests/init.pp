d88c96b6b4ef23dab32e779e4fe0de2407e517a5
Tracy Johnson <traj112@hotmail.com>
2020-06-09 20:19:52 -0500

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,5 @@ class exercise1 {
 	include exercise1::packages
 	include exercise1::users
 	include exercise1::disk_management::fs
+	include exercise1::mount
 }
--- manifests/mount.pp
+++ manifests/mount.pp
@@ -2,6 +2,7 @@
 #creating resources for mounting
 
 class exercise1::mount {
+	require exercise1::disk_management::fs
 	$mount = hiera('exercise1::directories')
 	create_resources(file, $mount)
 	
